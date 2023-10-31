0a04260ae2419f29c7a24217caffbb59834e823c
ankit kumar <103094580+Ankitkumar-git@users.noreply.github.com>
2023-10-31 23:46:10 +0530

    Add files via upload

---
+++ LAB 3 ap2/Exercise/package1/ClassA.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class ClassA
+{
+protected void displayA()
+{
+System.out.println("This is Class A form package1");
+}
+}
---
+++ LAB 3 ap2/Exercise/package2/ClassB.java
@@ -0,0 +1,9 @@
+package package2;
+
+public class ClassB
+{
+public void displayB()
+{
+System.out.println("This is ClassB from package2.");
+}
+}
